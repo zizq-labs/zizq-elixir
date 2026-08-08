4a9cad72fc111c95211b8b6056535448332707b6
Chris Corbyn <chris@w3style.co.uk>
2026-08-08 23:24:56 +1000

    Cut Release 0.6.0-alpha.2. (#10)
    See CHANGELOG for details. This includes enough to get some preliminary
    feedback even though all you can do right now is enqueue jobs.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Zizq.MixProject do
   use Mix.Project
 
-  @version "0.6.0-alpha.1"
+  @version "0.6.0-alpha.2"
   @source_url "https://github.com/zizq-labs/zizq-elixir"
 
   def project do

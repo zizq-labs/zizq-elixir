f08066a9568aaf22bc0dd1aa221d26f72cdc19c3
Chris Corbyn <chris@w3style.co.uk>
2026-08-10 21:14:42 +1000

    Bump to 0.6.0-alpha.4 (unstable pre-release). (#19)
    See CHANGELOG.md.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Zizq.MixProject do
   use Mix.Project
 
-  @version "0.6.0-alpha.3"
+  @version "0.6.0-alpha.4"
   @source_url "https://github.com/zizq-labs/zizq-elixir"
 
   def project do
